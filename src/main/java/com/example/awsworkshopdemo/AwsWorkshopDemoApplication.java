294be0b2435e3aacf0b41636ce3acc73ebee265b
sshyun <sshyun@amazon.com>
2022-09-01 16:40:28 +0900

    first commit

---
+++ src/main/java/com/example/awsworkshopdemo/AwsWorkshopDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsworkshopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsWorkshopDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsWorkshopDemoApplication.class, args);
+    }
+
+}
